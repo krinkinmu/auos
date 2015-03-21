545ae42ea7d0489da564dd61fd3b1b18bf0ebe9c
Mike Krinkin <krinkin.m.u@gmail.com>
2015-03-21 18:20:43 +0300

    Add code and data segments definitions.
    This patch adds boot time data and code segments definitions.

--- kernel/arch/x86/boot/boot.h
+++ kernel/arch/x86/boot/boot.h
@@ -10,5 +10,12 @@
 /* common boot definitions */
 #define BOOT_TIME_STACK_SIZE 0x4000
 
+#define BOOT_CS_ENTRY        1
+#define BOOT_CS              (BOOT_CS_ENTRY << 3)
+#define BOOT_DS_ENTRY        2
+#define BOOT_DS              (BOOT_DS_ENTRY << 3)
+
+#define PTE_INIT_ATTR        0x00000003
+#define PDE_INIT_ATTR        0x00000003
 
 #endif /*__BOOT_H__*/
