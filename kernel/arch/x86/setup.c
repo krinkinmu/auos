811181b46fb1746886869fb2d9b95917360291c0
Mike Krinkin <krinkin.m.u@gmail.com>
2015-03-22 18:11:44 +0300

    Add setup_arch function.
    So far function only prints memory map (and implicitly assumes, that
    we have 4GB of physical memory or less), but we will reload cr3 and
    gdt in this function also.

--- kernel/arch/x86/setup.c
+++ kernel/arch/x86/setup.c
@@ -1,11 +1,28 @@
 #include "multiboot.h"
 #include "io.h"
 
+void setup_arch(void *bootstrap)
+{
+	struct multiboot_info *mbi = bootstrap;
+	unsigned long mmap_entry = mbi->mmap_addr;
+	unsigned long mmap_end = mmap_entry + mbi->mmap_length;
+
+	while (mmap_entry < mmap_end) {
+		struct multiboot_mmap_entry *entry = (void *)mmap_entry;
+		unsigned long addr = entry->addr;
+		unsigned long size = entry->len;
+		unsigned long type = entry->type;
+
+		printf("memory region: 0x%x-0x%x type=%u\n",
+			addr, addr + size - 1, type);
+		mmap_entry += entry->size + sizeof(entry->size);
+	}
+}
+
 void main(void *bootstrap)
 {
-	(void)bootstrap;
-
 	init_io();
 	puts("Hello, World!");
+	setup_arch(bootstrap);
 	while (1);
 }
