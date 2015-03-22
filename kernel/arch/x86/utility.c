aa8d6fb28f49fc286b8d8a4d98c7c17e12594cc9
Mike Krinkin <krinkin.m.u@gmail.com>
2015-03-22 17:28:46 +0300

    Add number to string cinversion functions.
    I will use this functions to implement printf-like function.

--- kernel/arch/x86/utility.c
+++ kernel/arch/x86/utility.c
@@ -7,3 +7,32 @@ void * memset(void *ptr, int value, unsigned long size)
 
 	return ptr;
 }
+
+void ultoa(char *buf, unsigned long d, int base)
+{
+	char *begin = buf;
+	char *end = buf;
+
+	do {
+		unsigned rem = d % base;
+		*end++ = (rem < 10) ? rem + '0' : rem + 'a' - 10;
+	} while (d /= base);
+
+	*end-- = 0;
+	while (begin < end) {
+		char tmp = *begin;
+		*begin++ = *end;
+		*end-- = tmp;
+	}
+}
+
+void ltoa(char *buf, long d, int base)
+{
+	unsigned long n = d;
+
+	if (base == 10 && d < 0) {
+		*buf++ = '-';
+		n = (unsigned long)(-d);
+	}
+	ultoa(buf, n, base);
+}
--- kernel/arch/x86/utility.h
+++ kernel/arch/x86/utility.h
@@ -2,5 +2,7 @@
 #define __UTILITY_H__
 
 void * memset(void *ptr, int value, unsigned long size);
+void ultoa(char *buf, unsigned long d, int base);
+void ltoa(char *buf, long d, int base);
 
 #endif /*__UTILITY_H__*/
