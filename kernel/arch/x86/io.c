7726cca45b18c43fb393698e7149a9d07e1487ad
Mike Krinkin <krinkin.m.u@gmail.com>
2015-03-22 18:09:50 +0300

    Add printf-like function and fix nasty bug.
    This patch adds super simple printf-like function to io. This
    function is able to handle numbers, chars and null terminated one
    byte strings.

--- kernel/arch/x86/io.c
+++ kernel/arch/x86/io.c
@@ -1,8 +1,9 @@
+#include <stdarg.h>
 #include "utility.h"
 #include "io.h"
 
-static const unsigned ROWS = 80;
-static const unsigned COLS = 24;
+static const unsigned ROWS = 24;
+static const unsigned COLS = 80;
 static const char ATTR = 7;
 static char * const VMEM = (char *)0xB8000;
 
@@ -19,8 +20,10 @@ void putchar(int c)
 {
 	const unsigned shift = (col + row * COLS) * 2;
 
-	if (c == '\n')
+	if (c == '\n') {
 		newline();
+		return;
+	}
 
 	*(VMEM + shift) = c & 0xFF;
 	*(VMEM + shift + 1) = ATTR;
@@ -29,11 +32,51 @@ void putchar(int c)
 		newline();
 }
 
-void puts(const char *asciz)
+static void puts_no_newline(const char *asciz)
 {
 	while (*asciz)
 		putchar(*asciz++);
+}
+
+void puts(const char *asciz)
+{
+	puts_no_newline(asciz);
 	putchar('\n');
+}
+
+void printf(const char *fmt, ...)
+{
+	char buffer[20];
+	int c;
+
+	va_list vararg_lst;
+	va_start(vararg_lst, fmt);
+
+	while ((c = *fmt++) != 0) {
+		if (c != '%') {
+			putchar(c);
+			continue;
+		}
+
+		c = *fmt++;
+		if (c == 'u' || c == 'x') {
+			unsigned value = va_arg(vararg_lst, unsigned);
+			ultoa(buffer, value, (c == 'x' ? 16 : 10));
+			puts_no_newline(buffer);
+		} else if (c == 'd') {
+			int value = va_arg(vararg_lst, int);
+			ltoa(buffer, value, (c == 'x' ? 16 : 10));
+			puts_no_newline(buffer);
+		} else if (c == 's') {
+			puts_no_newline(va_arg(vararg_lst, const char *));
+		} else if (c == 'c') {
+			putchar(va_arg(vararg_lst, int));
+		} else {
+			putchar(c);
+		}
+	}
+
+	va_end(vararg_lst);
 }
 
 void init_io()
--- kernel/arch/x86/io.h
+++ kernel/arch/x86/io.h
@@ -3,6 +3,7 @@
 
 void putchar(int c);
 void puts(const char *asciz);
+void printf(const char *fmt, ...);
 void init_io();
 
 #endif /*__IO_H__*/
