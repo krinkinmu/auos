f1615a64d1a517a3b1835310efc6ed0e552d95d5
Mike Krinkin <krinkin.m.u@gmail.com>
2015-03-23 19:06:30 +0300

    Move layout.h to memory.h and add paging definitions.

--- kernel/include/arch/x86/layout.h
+++
@@ -1,9 +0,0 @@
-#ifndef __LAYOUT_H__
-#define __LAYOUT_H__
-
-#define PAGE_OFFSET 0xC0000000
-#define PAGE_SIZE   0x00001000
-
-#define pa(x) ((x) - PAGE_OFFSET)
-
-#endif /*__LAYOUT_H__*/
---
+++ kernel/include/arch/x86/memory.h
@@ -0,0 +1,34 @@
+#ifndef __MEMORY_H__
+#define __MEMORY_H__
+
+#define PAGE_OFFSET  0xC0000000
+#define PAGE_SHIFT   12
+#define PAGE_SIZE    (1 << 12)
+#define PGDIR_SHIFT  22
+#define PGDIR_SIZE   (1 << 22)
+#define PTRS_PER_PTE 1024
+#define PTRS_PER_PGD 1024
+#define pa(x)        ((x) - PAGE_OFFSET)
+
+#ifndef ASM_FILE
+#define pgd_index(ptr) \
+	(((unsigned long)(ptr) >> PGDIR_SHIFT) & (PTRS_PER_PGD - 1))
+
+#define KERNEL_PGD_BOUNDARY pgd_index(PAGE_OFFSET)
+#define KERNEL_PGD_PTRS     (PTRS_PER_PGD - KERNEL_PGD_BOUNDARY)
+#define phys_addr(x)        ((void *)((unsigned long)(x) - PAGE_OFFSET))
+
+typedef unsigned long pgdval_t;
+typedef unsigned long pteval_t;
+
+typedef struct { pgdval_t pgd; } pgd_t;
+typedef struct { pteval_t pte; } pte_t;
+
+extern pgd_t swapper_page_dir[PTRS_PER_PGD];
+extern pgd_t initial_page_dir[PTRS_PER_PGD];
+
+static inline void load_cr3(pgd_t *pde)
+{ __asm__ ("movl %0, %%cr3" : : "a"(pde)); }
+#endif /* ASM_FILE */
+
+#endif /*__MEMORY_H__*/
