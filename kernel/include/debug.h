b77ff1f84131d5ed80581bf3ca4c532060a35b9a
Mike Krinkin <krinkin.m.u@gmail.com>
2015-03-27 18:32:51 +0300

    Add helpful debug macroses.

---
+++ kernel/include/debug.h
@@ -0,0 +1,21 @@
+#ifndef __DEBUG_H__
+#define __DEBUG_H__
+
+#include <io.h>
+
+#ifdef NDEBUG
+#define debug(...)
+#else
+#define debug(...)                   \
+	do {                         \
+		printf(__VA_ARGS__); \
+	} while(0)
+#endif
+
+#define panic(...)                   \
+	do {                         \
+		printf(__VA_ARGS__); \
+		while (1);           \
+	} while (0)
+
+#endif /*__DEBUG_H__*/
