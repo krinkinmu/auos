07f1b9ed2496785c9ea831448b74c55f286a33fb
Mike Krinkin <krinkin.m.u@gmail.com>
2015-03-27 18:33:22 +0300

    Add pte/pde attribute flags.

--- kernel/include/arch/x86/cpu.h
+++ kernel/include/arch/x86/cpu.h
@@ -17,4 +17,21 @@
 #define X86_CR0_AM	BITUL(18)
 #define X86_CR0_PG	BITUL(31)
 
+#define X86_PTE_PRESENT   BITUL(0)
+#define X86_PTE_WRITE     BITUL(1)
+#define X86_PTE_USER      BITUL(2)
+#define X86_PTE_WRTHROUGH BITUL(3)
+#define X86_PTE_NOCACHE   BITUL(4)
+#define X86_PTE_ACCESSED  BITUL(5)
+#define X86_PTE_DIRTY     BITUL(6)
+#define X86_PTE_GLOBAL    BITUL(8)
+
+#define X86_PDE_PRESENT   BITUL(0)
+#define X86_PDE_WRITE     BITUL(1)
+#define X86_PDE_USER      BITUL(2)
+#define X86_PDE_WRTHROUGH BITUL(3)
+#define X86_PDE_NOCACHE   BITUL(4)
+#define X86_PDE_ACCESSED  BITUL(5)
+#define X86_PDE_LARGE     BITUL(7)
+
 #endif /*__CPU_H__*/
