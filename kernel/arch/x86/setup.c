dd109cfdcb4700e7c44542dd784a188f1d45e995
Mike Krinkin <krinkin.m.u@gmail.com>
2015-04-04 17:03:05 +0300

    Tiny test for slab allocator.

--- kernel/arch/x86/setup.c
+++ kernel/arch/x86/setup.c
@@ -1,4 +1,6 @@
-#include "memory.h"
+#include <kernel/slab_cache.h>
+#include <kernel/debug.h>
+#include <arch/memory.h>
 #include "multiboot.h"
 
 void setup_arch(void *bootstrap)
@@ -8,4 +10,17 @@ void setup_arch(void *bootstrap)
 	struct multiboot_info *mbi = bootstrap;
 	init_io();
 	setup_memory(mbi);
+
+	struct slab_cache cache;
+	slab_cache_create(&cache, sizeof(unsigned long), sizeof(unsigned long));
+
+	unsigned long *ulptr1 = slab_cache_alloc(&cache, ALLOC_NORMAL);
+	debug("Returned ptr 0x%x\n", (unsigned long)ulptr1);
+	unsigned long *ulptr2 = slab_cache_alloc(&cache, ALLOC_NORMAL);
+	debug("Returned ptr 0x%x\n", (unsigned long)ulptr2);
+
+	slab_cache_free(&cache, ulptr2);
+	slab_cache_free(&cache, ulptr1);
+
+	slab_cache_destroy(&cache);
 }
