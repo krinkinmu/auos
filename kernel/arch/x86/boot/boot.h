00fe7c0a95938d36fc22ef7498aec335e081533c
Mike Krinkin <krinkin.m.u@gmail.com>
2015-04-20 19:12:16 +0300

    fix code style in boot.h

--- kernel/arch/x86/boot/boot.h
+++ kernel/arch/x86/boot/boot.h
@@ -5,10 +5,10 @@
 #include <arch/asm/cpu.h>
 
 /* multiboot definitions */
-#define MB_HEADER_MAGIC 0x1BADB002
-#define MB_BOOT_MAGIC   0x2BADB002
-#define MB_PAGE_ALIGN   0x00000001
-#define MB_MEMORY_INFO  0x00000002
+#define MB_HEADER_MAGIC      0x1BADB002
+#define MB_BOOT_MAGIC        0x2BADB002
+#define MB_PAGE_ALIGN        0x00000001
+#define MB_MEMORY_INFO       0x00000002
 
 /* common boot definitions */
 #define BOOT_TIME_STACK_SIZE 0x4000
@@ -21,6 +21,6 @@
 #define PTE_INIT_ATTR        0x00000003
 #define PDE_INIT_ATTR        0x00000003
 
-#define pa(x) ((x) - PAGE_OFFSET)
+#define pa(x)                ((x) - PAGE_OFFSET)
 
 #endif /*__BOOT_H__*/
