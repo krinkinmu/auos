5984263579e8e07c4a9395799a82697ed42bf24a
Mike Krinkin <krinkin.m.u@gmail.com>
2015-04-25 13:21:46 +0300

    fix apic initialization

--- kernel/arch/x86/apic.c
+++ kernel/arch/x86/apic.c
@@ -1,21 +1,37 @@
 #include <kernel/acpi.h>
 #include <kernel/debug.h>
 #include <arch/apic.h>
+#include <arch/pic.h>
 
 #include <stddef.h>
 
 static struct io_apic io_apic[16];
 static size_t io_apic_size;
 
+static uint32_t io_apic_read(void *vaddr, unsigned reg)
+{
+	volatile uint32_t *io = vaddr;
+	io[0] = reg & 0xFFU;
+	return io[4];
+}
+
+static unsigned io_apic_irqs_count(void *vaddr)
+{
+	return 1 + ((io_apic_read(vaddr, IO_APIC_VERSION_REG) >> 16) & 0xFFU);
+}
+
 static void setup_io_apic_default(void)
 {
-	io_apic[0].vaddr = kmap(IO_APIC_DEFAULT_PADDR, IO_APIC_PAGE_FLAGS);
-	io_apic[0].id = 0xF & (io_apic_read(&io_apic[0], IO_APIC_ID_REG) >> 24);
-	io_apic[0].gsi_base = 0;
+	void *io_base = kmap(IO_APIC_DEFAULT_PADDR, IO_APIC_PAGE_FLAGS);
+	io_apic[0].vaddr = io_base;
+	io_apic[0].gsi_begin = 0;
+        io_apic[0].gsi_end = io_apic_irqs_count(io_base);
 	++io_apic_size;
 
-	debug("Default IO APIC at addr 0x%x id %u base %u\n",
-		IO_APIC_DEFAULT_PADDR, io_apic[0].id, io_apic[0].gsi_base);
+	debug("Default IO APIC at addr 0x%x irqs [%u, %u)\n",
+			IO_APIC_DEFAULT_PADDR,
+			io_apic[0].gsi_begin,
+			io_apic[0].gsi_end);
 }
 
 static int setup_io_apic_acpi(void)
@@ -35,29 +51,30 @@ static int setup_io_apic_acpi(void)
 	while (header < end) {
 		const struct acpi_irq_header *irq_hdr =
 					(const struct acpi_irq_header *)header;
+
 		header += irq_hdr->length;
 		if (irq_hdr->type != MADT_IO_APIC)
 			continue;
 
 		const struct acpi_io_apic *apic =
 					(const struct acpi_io_apic *)irq_hdr;
-		io_apic[io_apic_size].vaddr = kmap(apic->io_apic_paddr,
-					IO_APIC_PAGE_FLAGS);
-		io_apic[io_apic_size].id = apic->io_apic_id;
-		io_apic[io_apic_size].gsi_base = apic->gsi_base;
-		debug("Found IO APIC at addr 0x%x id %u base %u\n",
-			apic->io_apic_paddr, apic->io_apic_id, apic->gsi_base);
+
+		void *io_base = kmap(apic->io_apic_paddr, IO_APIC_PAGE_FLAGS);
+		const unsigned gsi_begin = apic->gsi_base;
+		const unsigned gsi_count = io_apic_irqs_count(io_base);
+
+		io_apic[io_apic_size].vaddr = io_base;
+		io_apic[io_apic_size].gsi_begin = gsi_begin;
+		io_apic[io_apic_size].gsi_end = gsi_begin + gsi_count;
+
+		debug("Found IO APIC at addr 0x%x base [%u, %u)\n",
+				apic->io_apic_paddr,
+				gsi_begin,
+				gsi_begin + gsi_count);
 		++io_apic_size;
 	}
 
 	return 1;
-}
-
-uint32_t io_apic_read(const struct io_apic *apic, int reg)
-{
-	volatile uint32_t *io = apic->vaddr;
-	io[0] = reg & 0xFF;
-	return io[4];
 }
 
 void setup_io_apic(void)
@@ -65,4 +82,7 @@ void setup_io_apic(void)
 	if (setup_io_apic_acpi())
 		return;
 	setup_io_apic_default();
+
+	pic_remap(0x20U);
+	pic_mask_interrupts(0xFFU);
 }
--- kernel/include/x86/arch/apic.h
+++ kernel/include/x86/arch/apic.h
@@ -5,15 +5,13 @@
 
 #define IO_APIC_DEFAULT_PADDR 0xFEC00000UL
 #define IO_APIC_PAGE_FLAGS    (PTE_KERNEL | PTE_READ | PTE_WRITE | PTE_NOCACHE)
-#define IO_APIC_ID_REG        0x00UL
+#define IO_APIC_VERSION_REG   0x01U
 
 struct io_apic {
 	void *vaddr;
-	uint32_t id;
-	uint32_t gsi_base;
+	unsigned gsi_begin;
+	unsigned gsi_end;
 };
-
-uint32_t io_apic_read(const struct io_apic *apic, int reg);
 
 void setup_io_apic(void);
 
