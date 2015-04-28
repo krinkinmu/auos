8e0f370a67cd8e505ff88aa4cd57d508aef1c401
Mike Krinkin <krinkin.m.u@gmail.com>
2015-04-28 21:55:34 +0300

    Initial setup of ISA interrupts

--- kernel/arch/x86/apic.c
+++ kernel/arch/x86/apic.c
@@ -10,26 +10,56 @@ static size_t apic_io_apic_count;
 static struct local_apic apic_local_apic[LOCAL_APIC_MAX_COUNT];
 static size_t apic_local_apic_count;
 
-static uint32_t apic_read(void *vaddr, unsigned reg)
+static struct isa_irq_map apic_isa_map[ISA_IRQ_MAX_COUNT];
+
+static uint32_t io_apic_read(void *vaddr, unsigned reg)
 {
 	volatile uint32_t *io = vaddr;
 	io[0] = reg & 0xFFU;
 	return io[4];
 }
 
+static void io_apic_write(void *vaddr, unsigned reg, uint32_t value)
+{
+	volatile uint32_t *io = vaddr;
+	io[0] = reg & 0xFFU;
+	io[4] = value;
+}
+
 static unsigned apic_io_apic_irqs(void *vaddr)
 {
-	return 1 + ((apic_read(vaddr, IO_APIC_VERSION_REG) >> 16) & 0xFFU);
+	return 1 + ((io_apic_read(vaddr, IO_APIC_VERSION_REG) >> 16) & 0xFFU);
 }
 
 static unsigned apic_io_apic_id(void *vaddr)
 {
-	return (apic_read(vaddr, IO_APIC_ID_REG) >> 24) & 0x0FU;
+	return (io_apic_read(vaddr, IO_APIC_ID_REG) >> 24) & 0x0FU;
 }
 
 static void apic_parse_irq_source_override(
 			const struct acpi_irq_source_override *descr)
 {
+	if (descr->source == descr->gsi && !(descr->flags &
+				(ISA_POLARITY_MASK | ISA_TRIGGER_MASK)))
+		return;
+
+	struct isa_irq_map map = {
+		descr->gsi,
+		IO_APIC_IRQ_DEST_LOGICAL | IO_APIC_IRQ_LOW_PRIORITY
+	};
+
+	if ((descr->flags & ISA_POLARITY_MASK) == ISA_POLARITY_LOW)
+		map.flags |= IO_APIC_IRQ_POLARITY_LOW;
+	else
+		map.flags |= IO_APIC_IRQ_POLARITY_HIGH;	
+
+	if ((descr->flags & ISA_TRIGGER_MASK) == ISA_TRIGGER_LEVEL)
+		map.flags |= IO_APIC_IRQ_TRIGGER_LEVEL;
+	else
+		map.flags |= IO_APIC_IRQ_TRIGGER_EDGE;
+
+	apic_isa_map[descr->source] = map;
+
 	debug("Standard ISA irq %u connected to %u gsi, flags %x\n",
 		descr->source,
 		descr->gsi,
@@ -105,11 +135,59 @@ static void apic_parse_madt(const struct acpi_madt_table *tbl)
 		apic_local_apic[i].vaddr = local_apic_vaddr;
 }
 
+static void apic_setup_irq(struct io_apic *apic, unsigned pin, unsigned dst,
+			unsigned long flags, unsigned cpus)
+{
+	const uint32_t low = (dst & 0xFFU) | (flags & 0xFF00UL);
+	const uint32_t high = ((uint32_t)cpus & 0xFFU) << 24;
+
+	io_apic_write(apic->vaddr, IO_APIC_REDTBL_REG + 2 * pin + 1, high);
+	io_apic_write(apic->vaddr, IO_APIC_REDTBL_REG + 2 * pin, low);
+}
+
+static void apic_setup_isa_irqs_default(void)
+{
+	for (unsigned i = 0; i != ISA_IRQ_MAX_COUNT; ++i) {
+		struct isa_irq_map map = {
+			i,
+			IO_APIC_IRQ_POLARITY_HIGH |
+			IO_APIC_IRQ_TRIGGER_EDGE |
+			IO_APIC_IRQ_DEST_LOGICAL |
+			IO_APIC_IRQ_LOW_PRIORITY
+		};
+		apic_isa_map[i] = map;
+	}
+}
+
+static struct io_apic *apic_find_gsi(unsigned gsi)
+{
+	for (unsigned i = 0; i != apic_io_apic_count; ++i) {
+		struct io_apic *apic = &apic_io_apic[i];
+		if (apic->gsi_begin <= gsi && apic->gsi_end > gsi)
+			return apic;
+	}
+
+	assert(0, "Unexpected GSI\n");
+	return 0;
+}
+
+static void apic_setup_irqs(void)
+{
+	for (unsigned i = 0; i != ISA_IRQ_MAX_COUNT; ++i) {
+		struct io_apic *apic = apic_find_gsi(apic_isa_map[i].gsi);
+		const unsigned pin = apic_isa_map[i].gsi - apic->gsi_begin;
+		apic_setup_irq(apic, pin, i + 0x20, apic_isa_map[i].flags,
+			IO_APIC_DEST_ALL);
+	}
+}
+
 void setup_apic(void)
 {
 	assert(acpi_available(), "ACPI inavailable!\n");
 	const struct acpi_madt_table *tbl =
 				(const void *)acpi_table_find(ACPI_MADT_SIGN);
 	assert(tbl, "There is no MADT!");
+	apic_setup_isa_irqs_default();
 	apic_parse_madt(tbl);
+	apic_setup_irqs();
 }
--- kernel/include/x86/arch/apic.h
+++ kernel/include/x86/arch/apic.h
@@ -9,9 +9,31 @@
 #define LOCAL_APIC_PAGE_FLAGS IO_APIC_PAGE_FLAGS
 #define IO_APIC_ID_REG        0x00U
 #define IO_APIC_VERSION_REG   0x01U
+#define IO_APIC_REDTBL_REG    0x10U
+
+#define IO_APIC_IRQ_POLARITY_HIGH  0x0UL
+#define IO_APIC_IRQ_POLARITY_LOW   BITUL(12)
+#define IO_APIC_IRQ_TRIGGER_EDGE   0x0UL
+#define IO_APIC_IRQ_TRIGGER_LEVEL  BITUL(15)
+#define IO_APIC_IRQ_DEST_LOGICAL   BITUL(11)
+#define IO_APIC_IRQ_LOW_PRIORITY   BITUL(8)
+#define IO_APIC_IRQ_MASKED         BITUL(16)
+#define IO_APIC_DEST_ALL    0xFFU
 
 #define IO_APIC_MAX_COUNT     0x10
 #define LOCAL_APIC_MAX_COUNT  0x08
+
+#define ISA_POLARITY_MASK     0x03
+#define ISA_POLARITY_DEFAULT  0x00
+#define ISA_POLARITY_HIGH     0x01
+#define ISA_POLARITY_LOW      0x03
+
+#define ISA_TRIGGER_MASK      0x0C
+#define ISA_TRIGGER_DEFAULT   0x00
+#define ISA_TRIGGER_EDGE      0x04
+#define ISA_TRIGGER_LEVEL     0x0C
+
+#define ISA_IRQ_MAX_COUNT     0x10
 
 struct io_apic {
 	void *vaddr;
@@ -23,6 +45,11 @@ struct io_apic {
 struct local_apic {
 	void *vaddr;
 	unsigned id;
+};
+
+struct isa_irq_map {
+	unsigned gsi;
+	unsigned long flags;
 };
 
 void setup_apic(void);
