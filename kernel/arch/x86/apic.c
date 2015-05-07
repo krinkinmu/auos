cfd0136921075705847caaa0ad0c7c0b90940b7f
Mike Krinkin <krinkin.m.u@gmail.com>
2015-05-07 18:46:35 +0300

    Refactor APIC code

--- kernel/arch/x86/apic.c
+++ kernel/arch/x86/apic.c
@@ -1,236 +1,389 @@
 #include <kernel/acpi.h>
 #include <kernel/debug.h>
+#include <kernel/irq.h>
+#include <kernel/kmap.h>
+#include <kernel/utility.h>
+
 #include <arch/apic.h>
 
-#include <stddef.h>
 
-static struct io_apic apic_io_apic[IO_APIC_MAX_COUNT];
-static size_t apic_io_apic_count;
+#define APIC_PAGE_FLAGS       (PTE_KERNEL | PTE_READ | PTE_WRITE | PTE_NOCACHE)
 
-static unsigned apic_local_apic_id[LOCAL_APIC_MAX_COUNT];
-static void *apic_local_apic_vaddr;
-static size_t apic_local_apic_count;
+#define ISA_IRQ_COUNT         16
+#define ISA_POLARITY_MASK     0x03U
+#define ISA_POLARITY_HIGH     0x01U
+#define ISA_POLARITY_LOW      0x03U
+#define ISA_TRIGGER_MASK      0x0CU
+#define ISA_TRIGGER_LEVEL     0x0CU
+#define ISA_TRIGGER_EDGE      0x04U
 
-static struct isa_irq_map apic_isa_map[ISA_IRQ_MAX_COUNT];
+#define IO_APIC_MAX_COUNT     32
+#define IO_APIC_ID_REG        0x00U
+#define IO_APIC_VER_REG       0x01U
+#define IO_APIC_REDTBL_REG    0x10U
+#define IO_APIC_PAGE_FLAGS    APIC_PAGE_FLAGS
+#define IO_APIC_DEST_ALL      0xFFU
+#define IO_APIC_LOW_PRI       BITUL(8)
+#define IO_APIC_LOGICAL       BITUL(11)
+#define IO_APIC_LEVEL         BITUL(15)
+#define IO_APIC_LOW           BITUL(13)
+#define IO_APIC_MASK          BITUL(16)
+
+#define LOCAL_APIC_MAX_COUNT  32
+#define LOCAL_APIC_ID_REG     0x08U
+#define LOCAL_APIC_LD_REG     0x34U
+#define LOCAL_APIC_SI_REG     0x3CU
+#define LOCAL_APIC_EOI_REG    0x2CU
+#define LOCAL_APIC_PAGE_FLAGS APIC_PAGE_FLAGS
+
+
+#define SPURIOUS_INTERRUPT_VECTOR 0xFFU
+
+
+struct io_apic {
+	void *iospace;
+	unsigned gsi;
+	unsigned irqs;
+	unsigned id;
+};
+
+static struct io_apic io_apic[IO_APIC_MAX_COUNT];
+static size_t io_apic_size;
+
+
+struct isa_irq_map {
+	struct io_apic *io_apic;
+	unsigned long flags;
+	unsigned gsi;
+	unsigned pin;
+};
+
+static struct isa_irq_map isa_irq[ISA_IRQ_COUNT];
+
+
+struct isa_irq_cfg {
+	unsigned long flags;
+	unsigned isa;
+	unsigned gsi;
+};
+
+struct local_apic {
+	unsigned pid;
+	unsigned lid;
+};
+
+static struct local_apic local_apic[LOCAL_APIC_MAX_COUNT];
+static void *local_apic_iospace;
+static size_t local_apic_count;
+
 
 static uint32_t io_apic_read(void *vaddr, unsigned reg)
 {
 	volatile uint32_t *io = vaddr;
-	io[0] = reg & 0xFFU;
-	return io[4];
+
+	*io = reg & 0xFFU;
+	return *(io + 4);
 }
 
 static uint32_t local_apic_read(unsigned reg)
 {
-	volatile uint32_t *io = apic_local_apic_vaddr;
+	volatile uint32_t *io = local_apic_iospace;
+
 	return io[reg];
 }
 
 static void io_apic_write(void *vaddr, unsigned reg, uint32_t value)
 {
 	volatile uint32_t *io = vaddr;
-	io[0] = reg & 0xFFU;
-	io[4] = value;
+
+	*io = reg & 0xFFU;
+	*(io + 4) = value;
 }
 
 static void local_apic_write(unsigned reg, uint32_t value)
 {
-	volatile uint32_t *io = apic_local_apic_vaddr;
+	volatile uint32_t *io = local_apic_iospace;
+
 	io[reg] = value;
 }
 
-static unsigned apic_io_apic_irqs(void *vaddr)
+static size_t io_apic_irqs(struct io_apic *apic)
 {
-	return 1 + ((io_apic_read(vaddr, IO_APIC_VR) >> 16) & 0xFFU);
+	const uint32_t version = io_apic_read(apic->iospace, IO_APIC_VER_REG);
+
+	return 1 + ((version >> 16) & 0xFFU);
 }
 
-static unsigned apic_io_apic_id(void *vaddr)
+static unsigned io_apic_id(struct io_apic *apic)
 {
-	return (io_apic_read(vaddr, IO_APIC_IDR) >> 24) & 0x0FU;
-}
-
-static void local_apic_set_lid(unsigned id)
-{
-	local_apic_write(LOCAL_APIC_LDR, (uint32_t)id << 24);
+	return (io_apic_read(apic->iospace, IO_APIC_ID_REG) >> 24) & 0x0FU;
 }
 
 static unsigned local_apic_pid(void)
 {
-	return local_apic_read(LOCAL_APIC_IDR) >> 24;
+	return local_apic_read(LOCAL_APIC_ID_REG) >> 24;
 }
 
-static void local_apic_set_spurious_vector(void)
+static struct io_apic *io_apic_find(unsigned gsi)
 {
-	const uint32_t siv = 0x13FFU;
-	local_apic_write(LOCAL_APIC_SIVR, siv);
-}
+	for (unsigned i = 0; i != io_apic_size; ++i) {
+		struct io_apic *apic = io_apic + i;
 
-static void apic_parse_irq_source_override(const struct acpi_isa_source *descr)
-{
-	if (descr->source == descr->gsi && !(descr->flags &
-				(ISA_POLARITY_MASK | ISA_TRIGGER_MASK)))
-		return;
-
-	struct isa_irq_map map = {
-		descr->gsi,
-		IO_APIC_IRQ_DEST_LOGICAL | IO_APIC_IRQ_LOW_PRIORITY
-	};
-
-	if ((descr->flags & ISA_POLARITY_MASK) == ISA_POLARITY_LOW)
-		map.flags |= IO_APIC_IRQ_POLARITY_LOW;
-	else
-		map.flags |= IO_APIC_IRQ_POLARITY_HIGH;	
-
-	if ((descr->flags & ISA_TRIGGER_MASK) == ISA_TRIGGER_LEVEL)
-		map.flags |= IO_APIC_IRQ_TRIGGER_LEVEL;
-	else
-		map.flags |= IO_APIC_IRQ_TRIGGER_EDGE;
-
-	if (descr->gsi < ISA_IRQ_MAX_COUNT)
-		apic_isa_map[descr->gsi].flags |= IO_APIC_IRQ_MASKED;
-
-	apic_isa_map[descr->source] = map;
-
-	debug("Standard ISA irq %u connected to %u gsi, flags %x\n",
-		descr->source,
-		descr->gsi,
-		descr->flags);
-}
-
-static void apic_parse_io_apic(const struct acpi_ioapic *apic)
-{
-	if (apic_io_apic_count == IO_APIC_MAX_COUNT) {
-		debug("Ignore IO APIC [%u]\n", apic->apic_id);
-		return;
-	}
-
-	void *io_base = kmap(apic->apic_paddr, IO_APIC_PAGE_FLAGS);
-	const unsigned gsi_begin = apic->gsi_base;
-	const unsigned gsi_end = gsi_begin + apic_io_apic_irqs(io_base);
-	const unsigned id = apic_io_apic_id(io_base);
-
-	assert(apic->apic_id == id,
-		"ACPI reported IO APIC id doesn't match IO APIC id\n");
-
-	apic_io_apic[apic_io_apic_count].vaddr = io_base;
-	apic_io_apic[apic_io_apic_count].gsi_begin = gsi_begin;
-	apic_io_apic[apic_io_apic_count].gsi_end = gsi_end;
-	apic_io_apic[apic_io_apic_count].id = id;
-	++apic_io_apic_count;
-
-	debug("Found IO APIC[%u] at addr 0x%x irqs [%u, %u)\n",
-		id,
-		apic->apic_paddr,
-		gsi_begin,
-		gsi_end);
-}
-
-static void apic_parse_local_apic(const struct acpi_lapic *apic)
-{
-	if (apic_local_apic_count == LOCAL_APIC_MAX_COUNT) {
-		debug("Ignore IO APIC [%u]\n", apic->apic_id);
-		return;
-	}
-
-	apic_local_apic_id[apic_local_apic_count] = apic->apic_id;
-	++apic_local_apic_count;
-	debug("Found Local APIC[%u]\n", apic->apic_id);
-}
-
-static void apic_parse_madt(const struct acpi_madt *tbl)
-{
-	const char *end = (const char *)tbl + tbl->header.length;
-	const char *irq_ptr = (const char *)(tbl + 1);
-
-	apic_local_apic_vaddr = kmap(tbl->lapic_paddr, LOCAL_APIC_PAGE_FLAGS);
-
-	while (irq_ptr < end) {
-		const struct acpi_madt_header *header = (void *)irq_ptr;
-
-		irq_ptr += header->length;
-		switch (header->type) {
-		case madt_ioapic:
-			apic_parse_io_apic((void *)header);
-			break;
-		case madt_lapic:
-			apic_parse_local_apic((void *)header);
-			break;
-		case madt_isa_source:
-			apic_parse_irq_source_override((void *)header);
-			break;
-		default:
-			debug("Unknown MADT header %u\n", header->type);
-			break;
-		}
-	}
-}
-
-static void apic_setup_irq(struct io_apic *apic, unsigned pin, unsigned dst,
-			unsigned long flags, unsigned cpus)
-{
-	const uint32_t low = (dst & 0xFFUL) | (flags & 0xFFF00UL);
-	const uint32_t high = ((uint32_t)cpus & 0xFFU) << 24;
-
-	io_apic_write(apic->vaddr, IO_APIC_REDTBLR + 2 * pin + 1, high);
-	io_apic_write(apic->vaddr, IO_APIC_REDTBLR + 2 * pin, low);
-}
-
-static void apic_setup_isa_irqs_default(void)
-{
-	for (unsigned i = 0; i != ISA_IRQ_MAX_COUNT; ++i) {
-		struct isa_irq_map map = {
-			i,
-			IO_APIC_IRQ_POLARITY_HIGH |
-			IO_APIC_IRQ_TRIGGER_EDGE |
-			IO_APIC_IRQ_DEST_LOGICAL |
-			IO_APIC_IRQ_LOW_PRIORITY
-		};
-		apic_isa_map[i] = map;
-	}
-}
-
-static struct io_apic *apic_find_gsi(unsigned gsi)
-{
-	for (unsigned i = 0; i != apic_io_apic_count; ++i) {
-		struct io_apic *apic = &apic_io_apic[i];
-		if (apic->gsi_begin <= gsi && apic->gsi_end > gsi)
+		if (apic->gsi <= gsi && apic->gsi + apic->irqs > gsi)
 			return apic;
 	}
-
-	assert(0, "Unexpected GSI\n");
+	panic("Cannot find IOAPIC for gsi %u\n", gsi);
 	return 0;
 }
 
-static void apic_setup_irqs(void)
+static void io_apic_setup_isa(const struct isa_irq_cfg *cfg)
 {
-	for (unsigned i = 0; i != ISA_IRQ_MAX_COUNT; ++i) {
-		struct io_apic *apic = apic_find_gsi(apic_isa_map[i].gsi);
-		const unsigned pin = apic_isa_map[i].gsi - apic->gsi_begin;
-		apic_setup_irq(apic, pin, i + 0x20, apic_isa_map[i].flags,
-			IO_APIC_DEST_ALL);
+	assert(cfg->isa < ISA_IRQ_COUNT, "Cannot remap non-ISA interrupts\n");
+	struct io_apic *apic = io_apic_find(cfg->gsi);
+
+	isa_irq[cfg->isa].gsi = cfg->gsi;
+	isa_irq[cfg->isa].io_apic = apic;
+	isa_irq[cfg->isa].pin = cfg->gsi - apic->gsi;
+	isa_irq[cfg->isa].flags = cfg->flags;
+}
+
+static void io_apic_isa_defaults(void)
+{
+	for (unsigned i = 0; i != ISA_IRQ_COUNT; ++i) {
+		struct isa_irq_cfg cfg = { i, i, HWIRQ_RISING };
+
+		io_apic_setup_isa(&cfg);
 	}
 }
 
+static void io_apic_add(unsigned long paddr, unsigned gsi)
+{
+	assert(io_apic_size < IO_APIC_MAX_COUNT, "Too many IOAPICs\n");
+
+	struct io_apic *apic = io_apic + io_apic_size;
+
+	apic->iospace = kmap(paddr, IO_APIC_PAGE_FLAGS);
+	apic->gsi = gsi;
+	apic->irqs = io_apic_irqs(apic);
+	apic->id = io_apic_id(apic);
+	++io_apic_size;
+}
+
+static void acpi_handle_io_apic(const struct acpi_madt_header *header)
+{
+	const struct acpi_io_apic *apic = (const void *)header;
+
+	if (io_apic_size == IO_APIC_MAX_COUNT) {
+		debug("Ignore IO APIC[%u]\n", apic->apic_id);
+		return;
+	}
+
+	io_apic_add(apic->apic_paddr, apic->gsi_base);
+}
+
+static void acpi_handle_isa_source(const struct acpi_madt_header *header)
+{
+	const struct acpi_isa_source *source = (const void *)header;
+	unsigned long flags = 0;
+	unsigned polarity = source->flags & ISA_POLARITY_MASK;
+	unsigned trigger = source->flags & ISA_TRIGGER_MASK;
+	struct isa_irq_cfg cfg;
+
+	if (source->source == source->gsi && !polarity && !trigger) {
+		debug("Useless ISA source override entry\n");
+		return;
+	}
+
+	memset(&cfg, 0, sizeof(cfg));
+
+	if (!polarity)
+		polarity = ISA_POLARITY_HIGH;
+
+	if (!trigger)
+		trigger = ISA_TRIGGER_EDGE;
+
+	if (trigger == ISA_TRIGGER_LEVEL)
+		flags |= (polarity == ISA_POLARITY_HIGH)
+			? HWIRQ_HIGH : HWIRQ_LOW;
+	else
+		flags |= (polarity == ISA_POLARITY_HIGH)
+			? HWIRQ_RISING : HWIRQ_FALLING;
+
+	cfg.isa = source->source;
+	cfg.gsi = source->gsi;
+	cfg.flags = flags;
+	io_apic_setup_isa(&cfg);
+}
+
+static void acpi_handle_local_apic(const struct acpi_madt_header *header)
+{
+	const struct acpi_local_apic *apic = (const void *)header;
+
+	if (local_apic_count == LOCAL_APIC_MAX_COUNT) {
+		debug("Too many Local APICs, ignore Local APIC[%u]\n",
+			apic->apic_id);
+		return;
+	}
+	debug("Fould Local APIC[%u]\n", apic->apic_id);
+	local_apic[local_apic_count++].pid = apic->apic_id;
+}
+
+static void setup_io_apic_acpi(void)
+{
+	acpi_parse_madt(MADT_IO_APIC, &acpi_handle_io_apic);
+	io_apic_isa_defaults();
+	acpi_parse_madt(MADT_ISA_SOURCE, &acpi_handle_isa_source);
+}
+
+static void setup_local_apic_acpi(void)
+{
+	const struct acpi_madt *tbl =
+		(const void *)acpi_table_find(ACPI_MADT_SIGN);
+
+	acpi_parse_madt(MADT_LOCAL_APIC, acpi_handle_local_apic);
+	local_apic_iospace = kmap(tbl->lapic_paddr, LOCAL_APIC_PAGE_FLAGS);
+}
+
+static void io_apic_setup_irq(struct io_apic *apic, unsigned pin, unsigned ino,
+			unsigned long flags, unsigned cpus)
+{
+	/*
+	 * interrupt vector number: [0  - 8)
+	 * delivery mode:           [8  - 11)
+	 * destination mode:        [11 - 12)
+	 * delivery status:         [12 - 13)
+	 * pin polarity:            [13 - 14)
+	 * IRR (not used):          [14 - 15)
+	 * trigger mode:            [15 - 15)
+	 * mask/unmask:             [16 - 17)
+	 * destination:             [56 - 64)
+	 */
+	const uint32_t high = (cpus & 0xFFU) << 24;
+	uint32_t low = (ino & 0xFFU) | IO_APIC_LOW_PRI | IO_APIC_LOGICAL;
+
+	if (flags & HWIRQ_DISABLED)
+		low |= IO_APIC_MASK;
+
+	if (flags & (HWIRQ_FALLING | HWIRQ_LOW))
+		low |= IO_APIC_LOW;
+
+	if (flags & (HWIRQ_HIGH | HWIRQ_LOW))
+		low |= IO_APIC_LEVEL;
+
+	io_apic_write(apic->iospace, IO_APIC_REDTBL_REG + 2 * pin + 1, high);
+	io_apic_write(apic->iospace, IO_APIC_REDTBL_REG + 2 * pin, low);
+}
+
+static void apic_setup(unsigned irq, unsigned long flags)
+{
+	struct io_apic *apic = 0;
+	unsigned pin = 0;
+	unsigned gsi = irq;
+
+	if (irq < ISA_IRQ_COUNT) {
+		if (flags)
+			debug("Request ISA irq with flags, flags ignored\n");
+
+		if (flags & HWIRQ_DISABLED)
+			flags = isa_irq[irq].flags | HWIRQ_DISABLED;
+		else
+			flags = isa_irq[irq].flags;
+
+		gsi = isa_irq[irq].gsi;
+		apic = isa_irq[irq].io_apic;
+		pin = isa_irq[irq].pin;
+	}
+
+	if (!apic) {
+		apic = io_apic_find(gsi);
+		pin = gsi - apic->gsi;
+	}
+
+	io_apic_setup_irq(apic, pin, gsi + HW_INTERRUPT_OFFSET, flags,
+		IO_APIC_DEST_ALL);
+}
+
+static void apic_mask(unsigned irq)
+{
+	struct io_apic *apic = 0;
+	unsigned pin = 0;
+	unsigned gsi = irq;
+
+	if (irq < ISA_IRQ_COUNT) {
+		gsi = isa_irq[irq].gsi;
+		apic = isa_irq[irq].io_apic;
+		pin = isa_irq[irq].pin;
+	}
+
+	if (!apic) {
+		apic = io_apic_find(gsi);
+		pin = gsi - apic->gsi;
+	}
+
+	const uint32_t low = io_apic_read(apic->iospace,
+		IO_APIC_REDTBL_REG + 2 * pin);
+
+	io_apic_write(apic->iospace, IO_APIC_REDTBL_REG + 2 * pin,
+		low | IO_APIC_MASK);
+}
+
+static void apic_unmask(unsigned irq)
+{
+	struct io_apic *apic = 0;
+	unsigned pin = 0;
+	unsigned gsi = irq;
+
+	if (irq < ISA_IRQ_COUNT) {
+		gsi = isa_irq[irq].gsi;
+		apic = isa_irq[irq].io_apic;
+		pin = isa_irq[irq].pin;
+	}
+
+	if (!apic) {
+		apic = io_apic_find(gsi);
+		pin = gsi - apic->gsi;
+	}
+
+	const uint32_t low = io_apic_read(apic->iospace,
+		IO_APIC_REDTBL_REG + 2 * pin);
+
+	io_apic_write(apic->iospace, IO_APIC_REDTBL_REG + 2 * pin,
+		low & ~IO_APIC_MASK);
+}
+
+static void apic_eoi(unsigned irq)
+{
+	(void) irq;
+	local_apic_write(LOCAL_APIC_EOI_REG, 0);
+}
+
+static const struct irqchip apic = {
+	.setup = &apic_setup,
+	.mask = &apic_mask,
+	.unmask = &apic_unmask,
+	.eoi = &apic_eoi
+};
+
 void setup_apic(void)
 {
-	assert(acpi_available(), "ACPI inavailable!\n");
-	const struct acpi_madt *tbl = (void *)acpi_table_find(ACPI_MADT_SIGN);
-	assert(tbl, "There is no MADT!\n");
-	apic_setup_isa_irqs_default();
-	apic_parse_madt(tbl);
-	apic_setup_irqs();
+	setup_io_apic_acpi();
+	setup_local_apic_acpi();
+	irqchip_register(&apic);
 }
 
 void setup_local_apic(void)
 {
 	const unsigned pid = local_apic_pid();
-	for (unsigned i = 0; i != apic_local_apic_count; ++i) {
-		if (pid == apic_local_apic_id[i]) {
-			local_apic_set_lid(1U << (i % 8));
-			local_apic_set_spurious_vector();
+
+	for (unsigned i = 0; i != local_apic_count; ++i) {
+		if (pid == local_apic[i].pid) {
+			const unsigned id = BITU(i % 8);
+
+			local_apic_write(LOCAL_APIC_LD_REG, id << 24);
+			local_apic_write(LOCAL_APIC_SI_REG,
+				SPURIOUS_INTERRUPT_VECTOR | BITU(8));
 			return;
 		}
 	}
-
-	panic("Local APIC[%u] not found!\n", pid);
+	panic("Cannot find Local APIC[%u]\n", pid);
 }
--- kernel/include/x86/arch/apic.h
+++ kernel/include/x86/arch/apic.h
@@ -1,54 +1,6 @@
 #ifndef __X86_ARCH_APIC_H__
 #define __X86_ARCH_APIC_H__
 
-#include <kernel/kmap.h>
-
-#define IO_APIC_DEFAULT_PADDR      0xFEC00000UL
-#define IO_APIC_DEFAULT_PADDR      0xFEC00000UL
-#define IO_APIC_PAGE_FLAGS         (PTE_KERNEL | PTE_READ | PTE_WRITE | \
-					PTE_NOCACHE)
-#define IO_APIC_IDR                0x00U
-#define IO_APIC_VR                 0x01U
-#define IO_APIC_REDTBLR            0x10U
-#define IO_APIC_IRQ_POLARITY_HIGH  0x0UL
-#define IO_APIC_IRQ_POLARITY_LOW   BITUL(12)
-#define IO_APIC_IRQ_TRIGGER_EDGE   0x0UL
-#define IO_APIC_IRQ_TRIGGER_LEVEL  BITUL(15)
-#define IO_APIC_IRQ_DEST_LOGICAL   BITUL(11)
-#define IO_APIC_IRQ_LOW_PRIORITY   BITUL(8)
-#define IO_APIC_IRQ_MASKED         BITUL(16)
-#define IO_APIC_DEST_ALL           0xFFU
-#define IO_APIC_MAX_COUNT          0x10U
-
-#define LOCAL_APIC_IDR             0x08U
-#define LOCAL_APIC_LDR             0x34U
-#define LOCAL_APIC_DFR             0x38U
-#define LOCAL_APIC_SIVR            0x3CU
-#define LOCAL_APIC_MAX_COUNT       0x08U
-#define LOCAL_APIC_PAGE_FLAGS      IO_APIC_PAGE_FLAGS
-
-#define ISA_POLARITY_MASK          0x03U
-#define ISA_POLARITY_DEFAULT       0x00U
-#define ISA_POLARITY_HIGH          0x01U
-#define ISA_POLARITY_LOW           0x03U
-#define ISA_TRIGGER_MASK           0x0CU
-#define ISA_TRIGGER_DEFAULT        0x00U
-#define ISA_TRIGGER_EDGE           0x04U
-#define ISA_TRIGGER_LEVEL          0x0CU
-#define ISA_IRQ_MAX_COUNT          0x10U
-
-struct io_apic {
-	void *vaddr;
-	unsigned gsi_begin;
-	unsigned gsi_end;
-	unsigned id;
-};
-
-struct isa_irq_map {
-	unsigned gsi;
-	unsigned long flags;
-};
-
 void setup_apic(void);
 void setup_local_apic(void);
 
